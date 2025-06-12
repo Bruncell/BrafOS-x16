1994a698d7c1eeaad912da8acc9681d5add6d5e0
Bruncell <bruncellq@gmail.com>
2025-06-12 10:15:30 +0300

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -8,7 +8,7 @@ mov al, 0x03
 int 0x10
 
 mov si, ART
-call print_string
+call print_string_color
 
 mov si, username
 call print_string
@@ -108,7 +108,7 @@ ret
 ;clear input buffer - - - - - - - - - - - - - -
 clear_input_buffer:
 
-.clear_loop
+.clear_loop:
 
 cmp si, input_buffer
 jbe .done_clear
@@ -143,9 +143,17 @@ jmp main_loop
 ;- - - - - - - - - - - - - - - - - - - - - - -
 help_:
 
+push si
+mov si, help_msg
+call print_string
+pop si
+call cursor_return
 ;- - - - - - - - - - - - - - - - - - - - - - - - - - - - -Тут будет функция хелп
-
 call clear_input_buffer
+
+
+
+
 
 
 
@@ -154,22 +162,38 @@ call clear_input_buffer
 
 
 print_bottom_panel:
-
+;потом надо переделать 
     pusha
     mov ax, 0xB800
     mov es, ax
     mov di, 3840  
     mov si, input_buffer
-new_char_bottom_panel:
+.new_char:
     lodsb
     test al, al
-    jz done_bottom_panel
+    jz .done
     mov ah, 0x1E 
     stosw
-    jmp new_char_bottom_panel
-done_bottom_panel:
+    jmp .new_char
+.done:
     popa
     ret
+
+cursor_return:
+pusha
+
+sub si, input_buffer
+mov bx, si
+
+mov ah, 0x02
+mov bh, 0x00
+mov dh, 9
+mov dl, 7
+add dl, bl
+int 0x10
+popa
+ret
+
  
 clear_bottom_panel:
     pusha
@@ -177,25 +201,39 @@ clear_bottom_panel:
     mov es, ax
     mov di, 3840
     mov cx, 80
-clear_bottom_panel_loop:
+.clear_loop:
     mov ax, 0x0720 
     mov ah, 0x1E
     stosw
-    loop clear_bottom_panel_loop
+    loop .clear_loop
+    popa
+    ret
+    
+print_string_color:
+    pusha
+.next_char:
+    lodsb
+    test al, al
+    jz .done
+    mov ah, 0x0E
+    mov bl, 0x0F
+    int 0x10
+    jmp .next_char
+.done:
     popa
     ret
 
 
 print_string:
     pusha
-print_string_next_char:
+.next_char:
     lodsb
     test al, al
-    jz done_print_string
+    jz .done
     mov ah, 0x0E
     int 0x10
-    jmp print_string_next_char
-done_print_string:
+    jmp .next_char
+.done:
     popa
     ret
 
@@ -208,7 +246,15 @@ help_command: db "help", 0
 ;----------
 
 ;msg
-help_msg: db "qwertyu"
+help_msg: 
+db 0Dh, 0Ah
+db 0Dh, 0Ah
+db "- - - - help panel - - - - ", 0Dh, 0Ah
+db "help messange 1", 0Dh, 0Ah
+db "help messange 2", 0Dh, 0Ah
+db "help messange 3", 0Dh, 0Ah
+db "help messange ...", 0Dh, 0Ah
+db "- - - - - - - - - - - - - - ", 0
 
 
 
@@ -219,14 +265,14 @@ clear_bottom_panel_text: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 ART:
 db 0Dh, 0Ah
-db " ____                      ___", 0Dh, 0Ah
-db "|  _ \\                   / _||", 0Dh, 0Ah
-db "| ||) ||  _ ___   __ __  | ||_     ___   ___", 0Dh, 0Ah
-db "|  _ <<  | '__|| / _` || |  _||   / _ \\/ __||", 0Dh, 0Ah
-db "| ||) || | ||   | ((| || | ||    | (() |\__ \\", 0Dh, 0Ah
-db "|____//  |_||    \__,_|| |_||     \___//|___//", 0Dh, 0Ah
+db "              ____                      ___", 0Dh, 0Ah
+db "             |  _ \\                   / _||", 0Dh, 0Ah
+db "             | ||) ||  _ ___   __ __  | ||_     ___   ___", 0Dh, 0Ah
+db "             |  _ <<  | '__|| / _` || |  _||   / _ \\/ __||", 0Dh, 0Ah
+db "             | ||) || | ||   | ((| || | ||    | (() |\__ \\", 0Dh, 0Ah
+db "             |____//  |_||    \__,_|| |_||     \___//|___//", 0Dh, 0Ah
 db 0Dh, 0Ah
-db "-------------------------------------------------------------------------------", 0Dh, 0Ah
+db "===============================================================================", 0Dh, 0Ah
 db 0
 
 input_buffer db 16 dup(0)
